c65a3628e716237f1cf7fdef849352569799be9c
Carlos Gallego <gallegomosquerajeancarlos@gmail.com>
2022-02-16 13:26:34 -0500

    Ui del Formulario de recuperación completado

--- src/app/modules/auth/account-recovery/account-recovery.component.ts
+++ src/app/modules/auth/account-recovery/account-recovery.component.ts
@@ -1,14 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './account-recovery.component.html',
   styleUrls: ['./account-recovery.component.scss']
 })
 export class AccountRecoveryComponent implements OnInit {
-
-  constructor() { }
+  public accountRecoveryForm: FormGroup;
+  constructor() {
+    this.accountRecoveryForm = new FormGroup({
+      'email': new FormControl('', [Validators.required, Validators.email])
+    })
+  }
 
   ngOnInit(): void {
+  }
+
+  onSubmitRecovery() {
+    console.log(this.accountRecoveryForm.value);
   }
 
 }
